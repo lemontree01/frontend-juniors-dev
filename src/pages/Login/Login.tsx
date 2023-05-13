2deaa9895a1d9e3824ecb96082b77a0445927b95
Maxim <maximtsoy2001@gmail.com>
2023-05-13 08:33:52 +0600

    redirec fix

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 
 const Login = () => {
+  console.log(process.env.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
   return (
     <div>
       <GoogleLogin
