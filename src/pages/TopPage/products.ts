24a17f39d3b5857e93ffb45efd281c4c4d39d30b
zhanybekk0v <zhanybekovnurbol@hmail.com>
2023-05-18 05:24:51 +0600

    top-page

---
+++ src/pages/TopPage/products.ts
@@ -0,0 +1,36 @@
+import simular1 from '../../assets/topPage/simular1.png'
+import simular2 from '../../assets/topPage/simular2.png'
+import simular3 from '../../assets/topPage/simular3.png'
+import simular4 from '../../assets/topPage/simular4.png'
+
+
+export const products = [
+  {
+    image: simular1,
+    id: 1,
+    paragraph: 'эпизод 5',
+    title: 'Ребёнок идола'
+  },
+  {
+    image: simular2,
+    id: 1,
+    paragraph: 'эпизод 8',
+    title: 'Идолмастер: Девушки-золушки — U149'
+
+  },
+  {
+    image: simular3,
+    id: 1,
+    paragraph: 'эпизод 6',
+    title: 'Юри — это моя работа!'
+
+  },
+  {
+    image: simular4,
+    id: 1,
+    paragraph: 'эпизод 5',
+    title: 'Я не посредственность, я просто дикий монстр'
+
+  },
+ 
+]
